f83f5fbe2cf5a97209e32158d422ee38f06f0c70
Sajid <yeamimhossainsajid@gmail.com>
2025-02-18 23:45:13 +0600

    first commit

---
+++ src/test/java/com/example/ChakriHub/ChakriHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChakriHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChakriHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
