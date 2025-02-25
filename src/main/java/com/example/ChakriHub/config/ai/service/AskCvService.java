1fe655d41f3dd8c5199257bdada77dd2738e9745
Sajid <yeamimhossainsajid@gmail.com>
2025-02-25 18:52:07 +0600

    Ai implementation  updated

--- src/main/java/com/example/ChakriHub/config/ai/service/AskCvService.java
+++ src/main/java/com/example/ChakriHub/config/ai/service/AskCvService.java
@@ -9,16 +9,16 @@ import org.springframework.stereotype.Service;
 public class AskCvService {
     @Autowired
     UserRepo userRepo;
-//    @Autowired
-//    CohereService cohereService;
     @Autowired
-    OpenRouterService openRouterService;
+    CohereService cohereService;
+//    @Autowired
+//    OpenRouterService openRouterService;
 
 
     public String askCv(Long id,String question) {
         User user = userRepo.findById(id).get();
         String cv=user.getCandidate().getCvInText();
-        String answer = openRouterService.getChatCompletion(cv+"         bro just read the full above text and based on that try to answer the below question.                  write within 3 lines       "+question);
+        String answer = cohereService.askCreator(cv+"         bro just read the full above text and think that it is you and based on that try to answer the below question.                write within 2 lines and fast and straight answer.no need additional talking  "+question);
         return answer;
     }
 
