2034df486e3a6630a97809d406c44e6fc57ee2ce
Sajid <yeamimhossainsajid@gmail.com>
2025-02-25 14:53:55 +0600

    Ai chating with CV added

---
+++ src/main/java/com/example/ChakriHub/config/ai/controller/AskCvController.java
@@ -0,0 +1,18 @@
+package com.example.ChakriHub.config.ai.controller;
+
+import com.example.ChakriHub.config.ai.service.AskCvService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("ai")
+public class AskCvController {
+    AskCvService askCvService;
+    public AskCvController(AskCvService askCvService) {
+        this.askCvService = askCvService;
+    }
+    @GetMapping("cv/question/{id}")
+    public ResponseEntity<String> askCv(@PathVariable Long id,@RequestBody String text) {
+        return ResponseEntity.ok(askCvService.askCv(id, text));
+    }
+}
---
+++ src/main/java/com/example/ChakriHub/config/ai/service/AskCvService.java
@@ -0,0 +1,24 @@
+package com.example.ChakriHub.config.ai.service;
+
+import com.example.ChakriHub.auth.model.User;
+import com.example.ChakriHub.auth.repository.UserRepo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AskCvService {
+    UserRepo userRepo;
+    CohereService cohereService;
+
+    public AskCvService(UserRepo userRepo, CohereService cohereService) {
+        this.userRepo = userRepo;
+        this.cohereService = cohereService;
+    }
+
+    public String askCv(Long id,String question) {
+        User user = userRepo.findById(id).get();
+        String cv=user.getCandidate().getCvInText();
+        String answer = cohereService.askCreator(cv+"         bro just read the full above text and based on that try to answer the below question.do answer long.try to keep it under 200 words or below"+question);
+        return answer;
+    }
+
+}
